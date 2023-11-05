c271409f40fb98246c31a0bb97e8634cbc4f62d2
amuwall <amuwall@outlook.com>
2023-11-05 16:10:49 +0800

    feat: add leetcode 501 answer

---
+++ leetcode501/leetcode501_test.go
@@ -0,0 +1,49 @@
+package leetcode501
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findMode(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "example 1",
+			args: args{
+				root: &TreeNode{
+					Val: 1,
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 2,
+						},
+					},
+				},
+			},
+			want: []int{2},
+		},
+		{
+			name: "example 2",
+			args: args{
+				root: &TreeNode{
+					Val: 0,
+				},
+			},
+			want: []int{0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMode(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findMode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
