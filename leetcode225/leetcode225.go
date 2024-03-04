f2b7451fde8253b266866afcae6e4a7901107d77
amuwall <amuwall@outlook.com>
2024-03-04 11:23:09 +0800

    feat: add leetcode 225 answer

---
+++ leetcode225/leetcode225.go
@@ -0,0 +1,43 @@
+package leetcode225
+
+type MyStack struct {
+	queue []int
+}
+
+func Constructor() MyStack {
+	return MyStack{
+		queue: []int{},
+	}
+}
+
+func (s *MyStack) Push(x int) {
+	n := len(s.queue)
+	s.queue = append(s.queue, x)
+	for ; n > 0; n-- {
+		s.queue = append(s.queue, s.queue[0])
+		s.queue = s.queue[1:]
+	}
+}
+
+func (s *MyStack) Pop() int {
+	x := s.queue[0]
+	s.queue = s.queue[1:]
+	return x
+}
+
+func (s *MyStack) Top() int {
+	return s.queue[0]
+}
+
+func (s *MyStack) Empty() bool {
+	return len(s.queue) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
